27425dbaab92d743d0983c0dbf389221c388f4fe
young97w <young97w@gmail.com>
2022-06-15 09:55:19 +0800

    procurement finish check

--- G1/finishCheck_procurement.js
+++ G1/finishCheck_procurement.js
@@ -31,22 +31,26 @@ function start(GUID) {
 function finish(GUID) {
     var entity = {};
     entity.statecode = 1;
-    
-    Xrm.WebApi.online.updateRecord("gppr2p_procurementrequest", GUID, entity).then(
-        function success(result) {
-            var updatedEntityId = result.id;
-            Xrm.Page.data.refresh();
-        },
-        function(error) {
-            Xrm.Utility.alertDialog(error.message);
+    para = "gppr2p_prodonlinenotifications?$filter=_gppr2p_procurementrequest_value eq " + GUID;
+    var results = retrieve(para);
+    if (results.value.length == 0) {
+        Xrm.Utility.alertDialog("请先生成上线检查任务！");
+    }else{
+        Xrm.WebApi.online.updateRecord("gppr2p_procurementrequest", GUID, entity).then(
+            function success(result) {
+                var updatedEntityId = result.id;
+                Xrm.Page.data.refresh();
+            },
+            function(error) {
+                Xrm.Utility.alertDialog(error.message);
+            }
+        );
         }
-    );
 }
 
-function retrieve(parameter, formContext, isAsy) {
-    var serverUrl = formContext.context.getClientUrl();
+function retrieve(parameter) {
     var req = new XMLHttpRequest();
-    req.open("GET", encodeURI(serverUrl + "/api/data/v9.1/" + parameter), isAsy);
+    req.open("GET", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/" + parameter, false);
     req.setRequestHeader("Accept", "application/json");
     req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
     req.setRequestHeader("OData-MaxVersion", "4.0");
@@ -55,28 +59,3 @@ function retrieve(parameter, formContext, isAsy) {
     req.send();
     return JSON.parse(req.responseText);
 }
-
-  
-para = "gppr2p_prodonlinenotifications?$filter=_gppr2p_procurementrequest_value eq "
-
-var req = new XMLHttpRequest();
-req.open("GET", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/", true);
-req.setRequestHeader("OData-MaxVersion", "4.0");
-req.setRequestHeader("OData-Version", "4.0");
-req.setRequestHeader("Accept", "application/json");
-req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
-req.onreadystatechange = function() {
-    if (this.readyState === 4) {
-        req.onreadystatechange = null;
-        if (this.status === 200) {
-            var results = JSON.parse(this.response);
-            for (var i = 0; i < results.value.length; i++) {
-                var activityid = results.value[i]["activityid"];
-            }
-        } else {
-            Xrm.Utility.alertDialog(this.statusText);
-        }
-    }
-};
-req.send();
